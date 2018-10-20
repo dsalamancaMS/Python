9cd59cb750041804ef1ae64d5e8c877f3ffc0bf8
dsalamancaMS <desalama@microsoft.com>
2018-10-20 10:54:25 -0600

    Added tests for pytest/tox

---
+++ Class2/test_division.py
@@ -0,0 +1,15 @@
+def test_division_entera():
+    num1 = 10
+    num2 = 3
+
+    my_div = num1 // num2
+    print(my_div)
+    assert 3 == my_div
+
+def test_division_exacta():
+    num1 = 10
+    num2 = 3
+
+    my_div = num1 / num2
+    print(my_div)
+    assert 3 == my_div
