4da98b0a932a1dc7cb0fa5db0f38091f9ac13cfe
dsalamancaMS <desalama@microsoft.com>
2018-10-31 08:08:15 -0600

    modified calc.py

--- Homeworks/1_Calculator/test_simple_calculator.py
+++ Homeworks/1_Calculator/test_simple_calculator.py
@@ -96,6 +96,19 @@ def main():
         print('\n',menu_operations[option])
         print('Result: ',menu_functions[option](),'\n')
 
+
+def test_add():
+    assert add(1,1) == 2
+
+def test_substract():
+    assert 0 == substract(1,1)
+
+def test_divide():
+    assert 2 == divide(10,5)
+
+def test_multiply():
+    assert 4 == multiply(2,2)
+
 main()
 
 
