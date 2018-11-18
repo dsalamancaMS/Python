8032d24a76297b10075a1c15ca60f0bfb2937eb6
dsalamancaMS <desalama@microsoft.com>
2018-11-17 20:36:17 -0600

    hello regex

--- Class4/asterics.py
+++ Class4/asterics.py
@@ -7,3 +7,7 @@ a = (1,2)
 # * unpacks values of tuple
 print(add(*a))
 
+# makes values into key=value pairs
+kargs = {'a':1, 'b':2}
+result = add(**kargs)
+print(a)
--- Class5/regex.py
+++ Class5/regex.py
@@ -14,3 +14,18 @@ if re.match(pattern,sequence):
 else:
     print("No match")
 
+email_address= 'Please contact us at: support_disk-denis@email.com'
+# it can be search of findall functions.
+# to replace you can use re.sub
+match= re.search(r'([\w\.\-\_]+)@([\w\.]+)', email_address)
+if match:
+    print(match.group())
+    print(match.group(1))
+    print(match.group(2))
+
+pattern_2 = "C"
+sequence1 = "IceCream"
+
+boolean_result = all([re.match(pattern, sequence1)])
+print(boolean_result)
+
